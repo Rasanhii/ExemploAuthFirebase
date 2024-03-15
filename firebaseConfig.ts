a0dd0540ce16d35513edac75aa88d36b9bc55566
Rasanhii <jonataspereira788@gmail.com>
2024-03-15 19:32:18 -0300

    Update firebaseConfig.ts
    Chave privadas em evidencia, cuidado com as mesmas

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARfZ0WKoRn-Cvx_Iq55QitLMEx1ThfJpE",
-  authDomain: "authproject-810dd.firebaseapp.com",
-  projectId: "authproject-810dd",
-  storageBucket: "authproject-810dd.appspot.com",
-  messagingSenderId: "364679702202",
-  appId: "1:364679702202:web:7e0f34045a73d6fc0e4483"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 export const FIREBASE_APP = initializeApp(firebaseConfig);
